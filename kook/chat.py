e254f9e51374a6c329ee1e2a8e73d80f54e7db55
Jerry Chen <jerryliang@jerryliang.top>
2024-03-08 15:34:30 +0800

    【聊天功能增强】添加频道内自动回复dify AI消息，支持开关控制

--- kook/chat.py
+++ kook/chat.py
@@ -1,5 +1,7 @@
-from khl import Bot, Message
+from khl import Bot, Message, EventTypes
 from chat.dify import dify
+
+channel_id == None
 
 
 def init(bot: Bot):
@@ -21,14 +23,34 @@ def init(bot: Bot):
     @bot.command(name="dify")
     async def dify_chat(msg: Message, ages: str):
         global channel_id
-        # 获取频道ID
-        channel_id = msg.ctx.channel.id
         if ages == "on":
             # 启动dify
+            # 获取频道ID
+            channel_id = msg.ctx.channel.id
             await msg.reply("已开启dify知识库的AI 聊天功能", use_quote=False)
         elif ages == "off":
             # 关闭dify
+            channel_id = None
             await msg.reply("已关闭dify知识库的AI 聊天功能", use_quote=False)
         else:
             # 未指定参数
             await msg.reply("未指定参数", use_quote=False)
+
+    # 监听该频道事件
+    @bot.on_message()
+    async def chat(msg: Message):
+        global channel_id
+        if msg.ctx.channel.id != channel_id:
+            return
+        # 实例化dify
+        chat_dify = dify()
+        if msg.content == "":
+            return
+        reply_list = []
+        reply = await chat_dify.ask(channel_id, msg.author, msg.content)
+        # 缓存reply 生成器的结果直到结束
+        async for i in reply:
+            reply_list.append(i)
+        # 将reply_list 转换成字符串
+        reply = "".join(reply_list)
+        await msg.reply(reply, use_quote=False)
